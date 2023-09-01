6c45d72aae9a8f1b45caebf5b16e1cc35051fdfd
RomanMadarash <98336444+RomanMadarash@users.noreply.github.com>
2023-09-01 17:49:06 +0300

    add delete task functionality

--- TaskManagement.Web/Models/TaskModel.cs
+++ TaskManagement.Web/Models/TaskModel.cs
@@ -4,6 +4,7 @@ namespace TaskManagement.Web.Models
 {
     public class TaskModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public Status Status { get; set; }
