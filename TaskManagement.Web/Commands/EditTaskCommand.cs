dc71de73e7ffcd1d651a9352beebc71f84e32b4c
RomanMadarash <98336444+RomanMadarash@users.noreply.github.com>
2023-09-21 16:26:01 +0300

    add mediatR

---
+++ TaskManagement.Web/Commands/EditTaskCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using System.Security.Claims;
+using TaskManagement.Persistence;
+using TaskManagement.Web.Models;
+
+namespace TaskManagement.Web.Commands
+{
+    public class EditTaskCommand : IRequest<Unit>
+    {
+        public ClaimsPrincipal User { get; set; }
+        public EditTaskModel EditTaskModel { get; set; }
+    }
+    public class EditTaskHandler : IRequestHandler<EditTaskCommand, Unit>
+    {
+        private readonly TaskManagementDbContext _dbContext;
+
+        private readonly IMapper _mapper;
+        public EditTaskHandler(TaskManagementDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(EditTaskCommand request, CancellationToken cancellationToken)
+        {
+            var task = await _dbContext.Tasks.FindAsync(request.EditTaskModel.Id);
+            if (task == null)
+            {
+                Exception exception;
+            }
+
+            _mapper.Map(request.EditTaskModel, task);
+
+            _dbContext.Tasks.Update(task);
+            await _dbContext.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+
+}
---
+++ TaskManagement.Web/Queryes/GetTasksQuery.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Security.Claims;
+using TaskManagement.Persistence;
+using TaskManagement.Web.Models;
+using TaskManagment.Domain;
+
+namespace TaskManagement.Web.Queryes
+{
+    public class GetTasksQuery : IRequest<List<TaskModel>>
+    {
+        public ClaimsPrincipal User { get; set; }
+        public string SearchValue { get; set; }
+
+    }
+    public class GetTasksHandler : IRequestHandler<GetTasksQuery, List<TaskModel>>
+    {
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly TaskManagementDbContext _dbContext;
+        private readonly IMediator _mediatR;
+        private readonly IMapper _mapper;
+        public GetTasksHandler(UserManager<UserEntity> userManager, IMediator mediatR, TaskManagementDbContext dbContext, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mediatR = mediatR;
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<List<TaskModel>> Handle(GetTasksQuery request, CancellationToken cancellationToken)
+        {
+            string search = null;
+            if (!string.IsNullOrEmpty(request.SearchValue))
+            {
+                search = request.SearchValue.ToLower();
+            }
+
+            var userName = _userManager.GetUserName(request.User);
+            var userTasks = await _dbContext
+                .Tasks
+                .Where(t => t.CreatedBy.Email == userName &&
+                    (string.IsNullOrEmpty(search) ||
+
+                    t.Name.ToLower().Contains(search) ||
+                    t.Description.ToLower().Contains(search))).OrderByDescending(d => d.EditedAt)
+                .ToListAsync();
+
+            var taskModels = _mapper.Map<List<TaskModel>>(userTasks);
+            return taskModels;
+        }
+    }
+}
